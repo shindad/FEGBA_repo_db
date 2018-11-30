04402df1fec4415b3494a4395f84266265cba456
Brady Shinners <brady.shinners@gmail.com>
2018-11-30 00:45:55 -0600

    commented

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ require("./controllers/routes/html-routes.js")(app);
 require("./controllers/routes/api-routes.js")(app);
 
 // Sync sequelize models and then start the Express app
- db.sequelize.sync({ force: true }).then(function () { //add force true for restarting db
-// db.sequelize.sync({}).then(function () {
+// db.sequelize.sync({ force: true }).then(function () { //add force true for restarting db
+db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 
     //Seeds to DB
-    require("./scripts/repoSeeder");
+    //require("./scripts/repoSeeder");
 
   });
 });
